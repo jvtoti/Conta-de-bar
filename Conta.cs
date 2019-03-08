bd6be4c5a9457ceb33ddba72ff69bc8f4c6ed5c3
jvtoti <48146417+jvtoti@users.noreply.github.com>
2019-03-08 15:16:23 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConta
+{
+    public class Conta
+    {
+        public decimal total;
+        public int quantidadePessoas;
+        public decimal totalPorPessoa;
+
+        public void Despesa(int qtdPessoa, decimal valorUnitario, int quantidade)
+        {
+            total += (valorUnitario * quantidade);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ClassConta;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsConta
+{
+    public partial class Form1 : Form
+    {
+        public Conta conta = new Conta();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal valorUnitario = Convert.ToDecimal(valorUnitarioTbx.Text);
+            int quantidade = Convert.ToInt32(quantidadeTbx.Text);
+            int qtdPessoa = Convert.ToInt32(qtdPessoasTbx.Text);
+            conta.Despesa(qtdPessoa, valorUnitario, quantidade);
+            totalLbl.Text = Convert.ToString(conta.total);
+            totalPorPessoasLbl.Text = (conta.total / qtdPessoa).ToString();
+
+
+            valorUnitarioTbx.Text = String.Empty;
+            quantidadeTbx.Text = "1";
+        }
+    }
+}
